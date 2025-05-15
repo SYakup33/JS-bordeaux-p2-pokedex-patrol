793797e24263718fad3a54c683af5f46610259f3
Yakup <yakup.senel33@gmail.com>
2025-05-15 16:46:30 +0200

    fixed resultscreen import

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.gif', '**/*.mp3', '**/*.svg'],
   // server: {
   //   proxy: {
   //     "/api": {
