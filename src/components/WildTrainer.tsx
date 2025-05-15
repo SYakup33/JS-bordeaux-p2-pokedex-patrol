5afb99e0c1098992589212102adfe698e74962fd
Yakup <yakup.senel33@gmail.com>
2025-05-15 18:20:15 +0200

    adding trainer image

--- src/components/WildTrainer.tsx
+++ src/components/WildTrainer.tsx
@@ -1,28 +1,33 @@
-interface TrainerInterface {
-	id: number;
-	declaredName: string;
-	cardName: string;
-	declaredRegion: string;
-	cardRegion: string;
-	portraitImage: string;
-	cardPortrait: string;
-	isTrainerCorrupted: boolean;
-}
+import "./WildTrainer.css";
+import chatBoxImg from "../assets/images/chat_box.svg";
+import trainer20imgfull from "../../assets/images/portraitImage/trainer20imgfull.svg";
+import trainer2imgfull from "../../assets/images/portraitImage/trainer2imgfull.svg";
+
+type TrainerImageKeys = "trainer20imgfull.svg" | "trainer2imgfull.svg";
 
 interface Props {
-	trainer: TrainerInterface;
+	trainer: {
+		id: number;
+		declaredName: string;
+		cardName: string;
+		declaredRegion: string;
+		cardRegion: string;
+		portraitImage: TrainerImageKeys;
+		cardPortrait: string;
+		isTrainerCorrupted: boolean;
+	};
 }
 
 function WildTrainer({ trainer }: Props) {
+	const trainerImages = {
+		"trainer20imgfull.svg": trainer20imgfull,
+		"trainer2imgfull.svg": trainer2imgfull,
+	};
 	return (
 		<div className="wildTrainer">
-			<img className="Trainer" src={trainer.portraitImage} alt="Trainer" />
+			<img src={trainerImages[trainer.portraitImage]} alt="Trainer" />
 			<div className="chatBox">
-				<img
-					className="trainerImg"
-					src="src/assets/images/chat_box.svg"
-					alt="chatBox"
-				/>
+				<img className="trainerImg" src={chatBoxImg} alt="chatBox" />
 				<p className="textOnImage">
 					{trainer.declaredName}: <br />
 					Salut je viens de {trainer.declaredRegion} !
